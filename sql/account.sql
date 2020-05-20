b216edf0248cb821b85be22e8769acc2ea507fc5
sunhong <1341484113@qq.com>
2020-05-20 19:59:30 +0800

    更新数据脚本文件

--- sql/account.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `account`(`name`, `money`) VALUES ('张三', 30000);
-INSERT INTO `account`(`name`, `money`) VALUES ('李四', -4000);
-INSERT INTO `account`(`name`, `money`) VALUES ('王五', 10000);
-INSERT INTO `account`(`name`, `money`) VALUES ('赵六', 10000);
-INSERT INTO `account`(`name`, `money`) VALUES ('孙七', 10000);
