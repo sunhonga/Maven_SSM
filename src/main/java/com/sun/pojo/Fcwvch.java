adf6b861dcfed16d446558475b2947c4b98af384
sunhong <1341484113@qq.com>
2020-05-22 15:15:18 +0800

    更新文件

---
+++ src/main/java/com/sun/pojo/Fcwvch.java
@@ -0,0 +1,393 @@
+package com.sun.pojo;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+/**
+ * @ClassName Fcwvch
+ * @Description TODO
+ * Author sunhong
+ * Date 2020-5-22 13:53
+ **/
+public class Fcwvch {
+
+    private Integer fterm;
+
+    private String fvchclsid;
+
+    private Long fvchpdh;
+
+    private Integer fvchbh;
+
+    private String fvchzy;
+
+    private String fkmh;
+
+    private String fcyid;
+
+    private BigDecimal frate;
+
+    private Short fyhdzbz;
+
+    private BigDecimal fbal;
+
+    private String fjd;
+
+    private BigDecimal fbbal;
+
+    private BigDecimal fsl;
+
+    private BigDecimal fbsl;
+
+    private BigDecimal fdj;
+
+    private Date fdate;
+
+    private Date fywdate;
+
+    private Integer ffjzs;
+
+    private String fzdr;
+
+    private String fcheckr;
+
+    private String fxgr;
+
+    private String fgzr;
+
+    private Short fgzbz;
+
+    private String fpzly;
+
+    private String fzqjyfs;
+
+    private String fmemo;
+
+    private Long fnumid;
+
+    private String fcashid;
+
+    private String fpz1;
+
+    private String fpz2;
+
+    private Integer ffromset;
+
+    private Short ftolevel;
+
+    private Short fupload;
+
+    private String fcpdm;
+
+    public Integer getFterm() {
+        return fterm;
+    }
+
+    public void setFterm(Integer fterm) {
+        this.fterm = fterm;
+    }
+
+    public String getFvchclsid() {
+        return fvchclsid;
+    }
+
+    public void setFvchclsid(String fvchclsid) {
+        this.fvchclsid = fvchclsid == null ? null : fvchclsid.trim();
+    }
+
+    public Long getFvchpdh() {
+        return fvchpdh;
+    }
+
+    public void setFvchpdh(Long fvchpdh) {
+        this.fvchpdh = fvchpdh;
+    }
+
+    public Integer getFvchbh() {
+        return fvchbh;
+    }
+
+    public void setFvchbh(Integer fvchbh) {
+        this.fvchbh = fvchbh;
+    }
+
+    public String getFvchzy() {
+        return fvchzy;
+    }
+
+    public void setFvchzy(String fvchzy) {
+        this.fvchzy = fvchzy == null ? null : fvchzy.trim();
+    }
+
+    public String getFkmh() {
+        return fkmh;
+    }
+
+    public void setFkmh(String fkmh) {
+        this.fkmh = fkmh == null ? null : fkmh.trim();
+    }
+
+    public String getFcyid() {
+        return fcyid;
+    }
+
+    public void setFcyid(String fcyid) {
+        this.fcyid = fcyid == null ? null : fcyid.trim();
+    }
+
+    public BigDecimal getFrate() {
+        return frate;
+    }
+
+    public void setFrate(BigDecimal frate) {
+        this.frate = frate;
+    }
+
+    public Short getFyhdzbz() {
+        return fyhdzbz;
+    }
+
+    public void setFyhdzbz(Short fyhdzbz) {
+        this.fyhdzbz = fyhdzbz;
+    }
+
+    public BigDecimal getFbal() {
+        return fbal;
+    }
+
+    public void setFbal(BigDecimal fbal) {
+        this.fbal = fbal;
+    }
+
+    public String getFjd() {
+        return fjd;
+    }
+
+    public void setFjd(String fjd) {
+        this.fjd = fjd == null ? null : fjd.trim();
+    }
+
+    public BigDecimal getFbbal() {
+        return fbbal;
+    }
+
+    public void setFbbal(BigDecimal fbbal) {
+        this.fbbal = fbbal;
+    }
+
+    public BigDecimal getFsl() {
+        return fsl;
+    }
+
+    public void setFsl(BigDecimal fsl) {
+        this.fsl = fsl;
+    }
+
+    public BigDecimal getFbsl() {
+        return fbsl;
+    }
+
+    public void setFbsl(BigDecimal fbsl) {
+        this.fbsl = fbsl;
+    }
+
+    public BigDecimal getFdj() {
+        return fdj;
+    }
+
+    public void setFdj(BigDecimal fdj) {
+        this.fdj = fdj;
+    }
+
+    public Date getFdate() {
+        return fdate;
+    }
+
+    public void setFdate(Date fdate) {
+        this.fdate = fdate;
+    }
+
+    public Date getFywdate() {
+        return fywdate;
+    }
+
+    public void setFywdate(Date fywdate) {
+        this.fywdate = fywdate;
+    }
+
+    public Integer getFfjzs() {
+        return ffjzs;
+    }
+
+    public void setFfjzs(Integer ffjzs) {
+        this.ffjzs = ffjzs;
+    }
+
+    public String getFzdr() {
+        return fzdr;
+    }
+
+    public void setFzdr(String fzdr) {
+        this.fzdr = fzdr == null ? null : fzdr.trim();
+    }
+
+    public String getFcheckr() {
+        return fcheckr;
+    }
+
+    public void setFcheckr(String fcheckr) {
+        this.fcheckr = fcheckr == null ? null : fcheckr.trim();
+    }
+
+    public String getFxgr() {
+        return fxgr;
+    }
+
+    public void setFxgr(String fxgr) {
+        this.fxgr = fxgr == null ? null : fxgr.trim();
+    }
+
+    public String getFgzr() {
+        return fgzr;
+    }
+
+    public void setFgzr(String fgzr) {
+        this.fgzr = fgzr == null ? null : fgzr.trim();
+    }
+
+    public Short getFgzbz() {
+        return fgzbz;
+    }
+
+    public void setFgzbz(Short fgzbz) {
+        this.fgzbz = fgzbz;
+    }
+
+    public String getFpzly() {
+        return fpzly;
+    }
+
+    public void setFpzly(String fpzly) {
+        this.fpzly = fpzly == null ? null : fpzly.trim();
+    }
+
+    public String getFzqjyfs() {
+        return fzqjyfs;
+    }
+
+    public void setFzqjyfs(String fzqjyfs) {
+        this.fzqjyfs = fzqjyfs == null ? null : fzqjyfs.trim();
+    }
+
+    public String getFmemo() {
+        return fmemo;
+    }
+
+    public void setFmemo(String fmemo) {
+        this.fmemo = fmemo == null ? null : fmemo.trim();
+    }
+
+    public Long getFnumid() {
+        return fnumid;
+    }
+
+    public void setFnumid(Long fnumid) {
+        this.fnumid = fnumid;
+    }
+
+    public String getFcashid() {
+        return fcashid;
+    }
+
+    public void setFcashid(String fcashid) {
+        this.fcashid = fcashid == null ? null : fcashid.trim();
+    }
+
+    public String getFpz1() {
+        return fpz1;
+    }
+
+    public void setFpz1(String fpz1) {
+        this.fpz1 = fpz1 == null ? null : fpz1.trim();
+    }
+
+    public String getFpz2() {
+        return fpz2;
+    }
+
+    public void setFpz2(String fpz2) {
+        this.fpz2 = fpz2 == null ? null : fpz2.trim();
+    }
+
+    public Integer getFfromset() {
+        return ffromset;
+    }
+
+    public void setFfromset(Integer ffromset) {
+        this.ffromset = ffromset;
+    }
+
+    @Override
+    public String toString() {
+        return "Fcwvch{" +
+                "fterm=" + fterm +
+                ", fvchclsid='" + fvchclsid + '\'' +
+                ", fvchpdh=" + fvchpdh +
+                ", fvchbh=" + fvchbh +
+                ", fvchzy='" + fvchzy + '\'' +
+                ", fkmh='" + fkmh + '\'' +
+                ", fcyid='" + fcyid + '\'' +
+                ", frate=" + frate +
+                ", fyhdzbz=" + fyhdzbz +
+                ", fbal=" + fbal +
+                ", fjd='" + fjd + '\'' +
+                ", fbbal=" + fbbal +
+                ", fsl=" + fsl +
+                ", fbsl=" + fbsl +
+                ", fdj=" + fdj +
+                ", fdate=" + fdate +
+                ", fywdate=" + fywdate +
+                ", ffjzs=" + ffjzs +
+                ", fzdr='" + fzdr + '\'' +
+                ", fcheckr='" + fcheckr + '\'' +
+                ", fxgr='" + fxgr + '\'' +
+                ", fgzr='" + fgzr + '\'' +
+                ", fgzbz=" + fgzbz +
+                ", fpzly='" + fpzly + '\'' +
+                ", fzqjyfs='" + fzqjyfs + '\'' +
+                ", fmemo='" + fmemo + '\'' +
+                ", fnumid=" + fnumid +
+                ", fcashid='" + fcashid + '\'' +
+                ", fpz1='" + fpz1 + '\'' +
+                ", fpz2='" + fpz2 + '\'' +
+                ", ffromset=" + ffromset +
+                ", ftolevel=" + ftolevel +
+                ", fupload=" + fupload +
+                ", fcpdm='" + fcpdm + '\'' +
+                '}';
+    }
+
+    public Short getFtolevel() {
+        return ftolevel;
+    }
+
+    public void setFtolevel(Short ftolevel) {
+        this.ftolevel = ftolevel;
+    }
+
+    public Short getFupload() {
+        return fupload;
+    }
+
+    public void setFupload(Short fupload) {
+        this.fupload = fupload;
+    }
+
+    public String getFcpdm() {
+        return fcpdm;
+    }
+
+    public void setFcpdm(String fcpdm) {
+        this.fcpdm = fcpdm == null ? null : fcpdm.trim();
+    }
+}
---
+++ src/main/java/com/sun/pojo/Jjhzhq.java
@@ -0,0 +1,273 @@
+package com.sun.pojo;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * @ClassName Jjhzhq
+ * @Description TODO
+ * Author sunhong
+ * Date 2020-5-22 13:52
+ **/
+public class Jjhzhq {
+
+    private Date fdate;
+
+    private String fzqdm;
+
+    @Override
+    public String toString() {
+        return "Jjhzhq{" +
+                "fdate=" + fdate +
+                ", fzqdm='" + fzqdm + '\'' +
+                ", fszsh='" + fszsh + '\'' +
+                ", fhqssj=" + fhqssj +
+                ", fhqpjj=" + fhqpjj +
+                ", fzrsp=" + fzrsp +
+                ", fjrkp=" + fjrkp +
+                ", fzgcj=" + fzgcj +
+                ", fzdcj=" + fzdcj +
+                ", fcjsl=" + fcjsl +
+                ", fcjje=" + fcjje +
+                ", fzf=" + fzf +
+                ", flybz='" + flybz + '\'' +
+                ", fjjdm='" + fjjdm + '\'' +
+                ", fsyqx=" + fsyqx +
+                ", fzzr='" + fzzr + '\'' +
+                ", fsh=" + fsh +
+                ", fchk='" + fchk + '\'' +
+                ", fjyzt='" + fjyzt + '\'' +
+                ", fzqmc='" + fzqmc + '\'' +
+                ", flybz1='" + flybz1 + '\'' +
+                ", fhqly='" + fhqly + '\'' +
+                ", fxzjq=" + fxzjq +
+                '}';
+    }
+
+    private String fszsh;
+
+    private BigDecimal fhqssj;
+
+    private BigDecimal fhqpjj;
+
+    private BigDecimal fzrsp;
+
+    private BigDecimal fjrkp;
+
+    private BigDecimal fzgcj;
+
+    private BigDecimal fzdcj;
+
+    private BigDecimal fcjsl;
+
+    private BigDecimal fcjje;
+
+    private BigDecimal fzf;
+
+    private String flybz;
+
+    private String fjjdm;
+
+    private Integer fsyqx;
+
+    private String fzzr;
+
+    private Integer fsh;
+
+    private String fchk;
+
+    private String fjyzt;
+
+    private String fzqmc;
+
+    private String flybz1;
+
+    private String fhqly;
+
+    private BigDecimal fxzjq;
+
+    public Date getFdate() {
+        return fdate;
+    }
+
+    public void setFdate(Date fdate) {
+        this.fdate = fdate;
+    }
+
+    public String getFzqdm() {
+        return fzqdm;
+    }
+
+    public void setFzqdm(String fzqdm) {
+        this.fzqdm = fzqdm == null ? null : fzqdm.trim();
+    }
+
+    public String getFszsh() {
+        return fszsh;
+    }
+
+    public void setFszsh(String fszsh) {
+        this.fszsh = fszsh == null ? null : fszsh.trim();
+    }
+
+    public BigDecimal getFhqssj() {
+        return fhqssj;
+    }
+
+    public void setFhqssj(BigDecimal fhqssj) {
+        this.fhqssj = fhqssj;
+    }
+
+    public BigDecimal getFhqpjj() {
+        return fhqpjj;
+    }
+
+    public void setFhqpjj(BigDecimal fhqpjj) {
+        this.fhqpjj = fhqpjj;
+    }
+
+    public BigDecimal getFzrsp() {
+        return fzrsp;
+    }
+
+    public void setFzrsp(BigDecimal fzrsp) {
+        this.fzrsp = fzrsp;
+    }
+
+    public BigDecimal getFjrkp() {
+        return fjrkp;
+    }
+
+    public void setFjrkp(BigDecimal fjrkp) {
+        this.fjrkp = fjrkp;
+    }
+
+    public BigDecimal getFzgcj() {
+        return fzgcj;
+    }
+
+    public void setFzgcj(BigDecimal fzgcj) {
+        this.fzgcj = fzgcj;
+    }
+
+    public BigDecimal getFzdcj() {
+        return fzdcj;
+    }
+
+    public void setFzdcj(BigDecimal fzdcj) {
+        this.fzdcj = fzdcj;
+    }
+
+    public BigDecimal getFcjsl() {
+        return fcjsl;
+    }
+
+    public void setFcjsl(BigDecimal fcjsl) {
+        this.fcjsl = fcjsl;
+    }
+
+    public BigDecimal getFcjje() {
+        return fcjje;
+    }
+
+    public void setFcjje(BigDecimal fcjje) {
+        this.fcjje = fcjje;
+    }
+
+    public BigDecimal getFzf() {
+        return fzf;
+    }
+
+    public void setFzf(BigDecimal fzf) {
+        this.fzf = fzf;
+    }
+
+    public String getFlybz() {
+        return flybz;
+    }
+
+    public void setFlybz(String flybz) {
+        this.flybz = flybz == null ? null : flybz.trim();
+    }
+
+    public String getFjjdm() {
+        return fjjdm;
+    }
+
+    public void setFjjdm(String fjjdm) {
+        this.fjjdm = fjjdm == null ? null : fjjdm.trim();
+    }
+
+    public Integer getFsyqx() {
+        return fsyqx;
+    }
+
+    public void setFsyqx(Integer fsyqx) {
+        this.fsyqx = fsyqx;
+    }
+
+    public String getFzzr() {
+        return fzzr;
+    }
+
+    public void setFzzr(String fzzr) {
+        this.fzzr = fzzr == null ? null : fzzr.trim();
+    }
+
+    public Integer getFsh() {
+        return fsh;
+    }
+
+    public void setFsh(Integer fsh) {
+        this.fsh = fsh;
+    }
+
+    public String getFchk() {
+        return fchk;
+    }
+
+    public void setFchk(String fchk) {
+        this.fchk = fchk == null ? null : fchk.trim();
+    }
+
+    public String getFjyzt() {
+        return fjyzt;
+    }
+
+    public void setFjyzt(String fjyzt) {
+        this.fjyzt = fjyzt == null ? null : fjyzt.trim();
+    }
+
+    public String getFzqmc() {
+        return fzqmc;
+    }
+
+    public void setFzqmc(String fzqmc) {
+        this.fzqmc = fzqmc == null ? null : fzqmc.trim();
+    }
+
+    public String getFlybz1() {
+        return flybz1;
+    }
+
+    public void setFlybz1(String flybz1) {
+        this.flybz1 = flybz1 == null ? null : flybz1.trim();
+    }
+
+    public String getFhqly() {
+        return fhqly;
+    }
+
+    public void setFhqly(String fhqly) {
+        this.fhqly = fhqly == null ? null : fhqly.trim();
+    }
+
+    public BigDecimal getFxzjq() {
+        return fxzjq;
+    }
+
+    public void setFxzjq(BigDecimal fxzjq) {
+        this.fxzjq = fxzjq;
+    }
+
+}
